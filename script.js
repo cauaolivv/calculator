24be19322498630203a0fd57292f5875c1177de4
cauaolivv <caua.rodrigues.oliveira@escola.pr.gov.br>
2024-09-03 10:45:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function calcula(opera){ 
+    let num1= parseFloat(document.getElementById(num1).value);
+    let num2= parseFloat(document.getElementById(num2).value);
+    let reultado= 0;
+    if(isNaN(num1) || isNaN(num2)){
+    alert ()
+    }else{
+
+    if(opera)
+}  
+}
+
+
